e0626af8a9d2e3630ec67f06d147a820c6304d0a
Anthoni Mathias <anthonimathias@gmail.com>
2021-04-16 10:02:09 +0100

    Add owl carousel in main js

--- js/main.js
+++ js/main.js
@@ -19,4 +19,20 @@ $(document).ready(function () {
 
     $('.project-area .button-group #btn1').trigger('click');
 
+    //Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            544: {
+                items:2
+            }
+        }
+    })
+
 });
